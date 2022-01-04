cf13b19c19d702241b9f71ccbd09479e988e833d
unknown <ee20btech11013@iith.ac.in>
2022-01-05 00:40:11 +0530

    Added

--- src/app/features/mess-council/mess-council.component.ts
+++ src/app/features/mess-council/mess-council.component.ts
@@ -8,22 +8,114 @@ import { Component, OnInit } from '@angular/core';
 export class MessCouncilComponent implements OnInit {
 
   constructor() { }
-  messInspection:Boolean=false
-  messCoordinator:Boolean=true
+  messInspection: Boolean = false
+  messCoordinator: Boolean = false
+  messhygene: Boolean = false
+  messprep: Boolean = false
+  canteenhygene: Boolean = false
+  messgriev: Boolean = false
+  cinspect: Boolean = false
+  cgriev: Boolean = false
+  cmcord: Boolean = false
   ngOnInit(): void {
   }
-  coord(){
-    this.messCoordinator=true;
-    this.messInspection=false;
+  coord() {
+    this.messCoordinator = true;
+    this.cmcord = false;
+    this.messprep = false;
+    this.messhygene = false;
+    this.messInspection = false;
+    this.canteenhygene = false;
+    this.messgriev = false;
+    this.cinspect = false;
+    this.cgriev = false;
   }
-  prep(){
-
+  prep() {
+    this.messCoordinator = false;
+    this.cmcord = false;
+    this.messprep = true;
+    this.messhygene = false;
+    this.messInspection = false;
+    this.canteenhygene = false;
+    this.messgriev = false;
+    this.cinspect = false;
+    this.cgriev = false;
   }
-  hygene(){
-
+  hygene() {
+    this.messCoordinator = false;
+    this.cmcord = false;
+    this.messprep = false;
+    this.messhygene = true;
+    this.messInspection = false;
+    this.canteenhygene = false;
+    this.messgriev = false;
+    this.cinspect = false;
+    this.cgriev = false;
   }
-  inspect(){
-    this.messCoordinator=false;
-    this.messInspection=true;
+  inspect() {
+    this.messCoordinator = false;
+    this.cmcord = false;
+    this.messprep = false;
+    this.messhygene = false;
+    this.messInspection = true;
+    this.canteenhygene = false;
+    this.messgriev = false;
+    this.cinspect = false;
+    this.cgriev = false;
+  }
+  chygene() {
+    this.messCoordinator = false;
+    this.cmcord = false;
+    this.messprep = false;
+    this.messhygene = false;
+    this.messInspection = false;
+    this.canteenhygene = true;
+    this.messgriev = false;
+    this.cinspect = false;
+    this.cgriev = false;
+  }
+  griev() {
+    this.messCoordinator = false;
+    this.cmcord = false;
+    this.messprep = false;
+    this.messhygene = false;
+    this.messInspection = false;
+    this.canteenhygene = false;
+    this.messgriev = true;
+    this.cinspect = false;
+    this.cgriev = false;
+  }
+  cinsp() {
+    this.messCoordinator = false;
+    this.cmcord = false;
+    this.messprep = false;
+    this.messhygene = false;
+    this.messInspection = false;
+    this.canteenhygene = false;
+    this.messgriev = false;
+    this.cinspect = true;
+    this.cgriev = false;
+  }
+  cgri() {
+    this.messCoordinator = false;
+    this.cmcord = false;
+    this.messprep = false;
+    this.messhygene = false;
+    this.messInspection = false;
+    this.canteenhygene = false;
+    this.messgriev = false;
+    this.cinspect = false;
+    this.cgriev = true;
+  }
+  cmc() {
+    this.messCoordinator = false;
+    this.cmcord = true;
+    this.messprep = false;
+    this.messhygene = false;
+    this.messInspection = false;
+    this.canteenhygene = false;
+    this.messgriev = false;
+    this.cinspect = false;
+    this.cgriev = false;
   }
 }
