feb5de11a61305ef6a3113e44a6ce7db49262463
sayeed <cs19btech11004@iith.ac.in>
2022-01-15 23:13:49 +0530

    archives

--- src/app/features/archives/team21/team21.component.ts
+++ src/app/features/archives/team21/team21.component.ts
@@ -11,5 +11,20 @@ export class Team21Component implements OnInit {
 
   ngOnInit(): void {
   }
-
+  president1:string = "../../../../assets/images/elections21-22/President.jpg"
+  president2:string = "../../../../assets/images/elections21/Hari_Om_Singh _President_1.jpg"
+  aas1:string = "../../../../assets/images/elections21-22/Acad.jpg"
+  aas2:string = "../../../../assets/images/elections21/Hari_Om_Singh _President_1.jpg"
+  gnII1:string = "../../../../assets/images/elections21-22/gen2.jpg"
+  gnI1:string = "../../../../assets/images/elections21-22/gen.jpeg"
+  sws1:string = "../../../../assets/images/elections21-22/welfare.jpg"
+  cults1:string = "../../../../assets/images/elections21-22/cultural.jpg"
+  sports1:string = "../../../../assets/images/elections21-22/sport.jpeg"
+  sci1:string = "../../../../assets/images/elections21-22/sci.jpeg"
+  mess1:string = "../../../../assets/images/elections21-22/mess.jpg"
+  ugrepI1:string = "../../../../assets/images/elections21-22/ug1.jpeg"
+  ugrepII1:string = "../../../../assets/images/elections21-22/ug2.jpg"
+  pgrepI1:string = "../../../../assets/images/elections21-22/pg1.jpeg"
+  pgrepII1:string = "../../../../assets/images/elections21-22/pg2.jpeg"
+  docrepI1:string = "../../../../assets/images/elections21-22/phd.jpg"
 }
