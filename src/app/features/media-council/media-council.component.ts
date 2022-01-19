f7909c63734f2921a53e6383ed8bb6ef26056c3d
unknown <ee20btech11013@iith.ac.in>
2022-01-19 23:22:57 +0530

    added

--- src/app/features/media-council/media-council.component.ts
+++ src/app/features/media-council/media-council.component.ts
@@ -8,8 +8,46 @@ import { Component, OnInit } from '@angular/core';
 export class MediaCouncilComponent implements OnInit {
 
   constructor() { }
-
+  web: Boolean = false
+  content: Boolean = false
+  digital: Boolean = false
+  visual: Boolean = false
+  photography: Boolean = false
   ngOnInit(): void {
   }
-
+  webt() {
+    this.web = true
+    this.content = false
+    this.digital = false
+    this.visual = false
+    this.photography = false
+  }
+  digitalt() {
+    this.web = false
+    this.content = false
+    this.digital = true
+    this.visual = false
+    this.photography = false
+  }
+  visualt() {
+    this.web = false
+    this.content = false
+    this.digital = false
+    this.visual = true
+    this.photography = false
+  }
+  contentt() {
+    this.web = false
+    this.content = true
+    this.digital = false
+    this.visual = false
+    this.photography = false
+  }
+  photot() {
+    this.web = false
+    this.content = false
+    this.digital = false
+    this.visual = false
+    this.photography = true
+  }
 }
