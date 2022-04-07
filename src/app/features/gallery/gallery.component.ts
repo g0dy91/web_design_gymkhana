1f481536be95fa4b9ce592761f4895c53552cdd9
sayeed <cs19btech11004@iith.ac.in>
2022-04-07 16:45:47 +0530

    added lazy loading

--- src/app/features/gallery/gallery.component.ts
+++ src/app/features/gallery/gallery.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent implements OnInit {
-
+  defaultImage = '../../../assets/images/gallery/200.gif';
   constructor() { }
 
   ngOnInit(): void {
