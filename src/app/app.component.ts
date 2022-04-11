84ba16f52a086cedd0df756fb97f469e1ad2ebf0
devulapallisai <ee20btech11013@iith.ac.in>
2022-04-12 01:56:08 +0530

    added  loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+// import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NgxUiLoaderService } from "ngx-ui-loader"; // Import NgxUiLoaderService
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'annoor-ui';
+  constructor(private ngxService: NgxUiLoaderService) { }
+  ngOnInit() {
+    this.ngxService.start(); // start foreground spinner of the master loader with 'default' taskId
+    // Stop the foreground loading after 5s
+    setTimeout(() => {
+      this.ngxService.stop(); // stop foreground spinner of the master loader with 'default' taskId
+    }, 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -10,6 +9,11 @@ import { BootstrapModule } from './core/module/bootstrap.module';
 import { FeaturesModule } from './features/features.module';
 import { SharedModule } from './shared/shared.module';
 import { LazyLoadImageModule } from 'ng-lazyload-image';
+import { NgxUiLoaderHttpConfig, NgxUiLoaderHttpModule, NgxUiLoaderModule } from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig: NgxUiLoaderHttpConfig = {
+};
+
 
 @NgModule({
   declarations: [
@@ -25,9 +29,18 @@ import { LazyLoadImageModule } from 'ng-lazyload-image';
     BootstrapModule,
     HttpClientModule,
     LazyLoadImageModule,
-
+    NgxUiLoaderModule,
+    NgxUiLoaderHttpModule.forRoot(
+      {
+        showForeground:true
+      }
+    )
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
+
 export class AppModule { }
